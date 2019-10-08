db1fc8e39c2e6aad90de9cf47b613b7fadf2eea0
Yaming-WBJM <51696564+Yaming-WBJM@users.noreply.github.com>
2019-10-08 14:43:54 +0800

    Add files via upload

---
+++ ctrlDoc.java
@@ -0,0 +1,87 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.docs.v1.Docs;
+import com.google.api.services.docs.v1.DocsScopes;
+import com.google.api.services.docs.v1.model.BatchUpdateDocumentRequest;
+import com.google.api.services.docs.v1.model.BatchUpdateDocumentResponse;
+import com.google.api.services.docs.v1.model.Document;
+import com.google.api.services.docs.v1.model.InsertTextRequest;
+import com.google.api.services.docs.v1.model.Location;
+import com.google.api.services.docs.v1.model.Request;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ctrlDoc {
+    private static final String APPLICATION_NAME = "Control Google Docs";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private static final List<String> SCOPES = Collections.singletonList(DocsScopes.DOCUMENTS);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = DocsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receier = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receier).authorize("user");
+    }
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Docs service = new Docs.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        Document doc = new Document()
+                .setTitle("My Document");
+        doc = service.documents().create(doc)
+                .execute();
+        System.out.println("Created document with title: " + doc.getTitle());
+        System.out.println("Document ID: " + doc.getDocumentId());
+        String DOCUMENT_ID = doc.getDocumentId();
+        
+        List<Request> requests = new ArrayList<>();
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText("Hello Google!\n")
+                .setLocation(new Location().setIndex(1))));
+
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText("This text was input through Google Docs API.\n")
+                .setLocation(new Location().setIndex(15))));
+
+        requests.add(new Request().setInsertText(new InsertTextRequest()
+                .setText("See you!")
+                .setLocation(new Location().setIndex(60))));
+
+        BatchUpdateDocumentRequest body = new BatchUpdateDocumentRequest().setRequests(requests);
+        BatchUpdateDocumentResponse response = service.documents()
+                .batchUpdate(DOCUMENT_ID, body).execute();
+    }
+}
